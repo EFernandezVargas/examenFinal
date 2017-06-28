145039a2beb3671f4894cf5d74c0d37fb1c25690
EFernandezVargas <guerra.fervar@gmail.com>
2017-06-27 20:51:07 -0400

    proyecto final de ACO

---
+++ ACO/controllers/reclamoController.js
@@ -0,0 +1,50 @@
+var models = require('../models/models.js');
+
+var hoy = new Date();
+var dd = hoy.getDate();
+var mm = hoy.getMonth()+1;
+var yyyy = hoy.getFullYear();
+hoy = yyyy+'-'+mm+'-'+dd;
+
+exports.principal = function(req, res, next) {
+                 models.reclamo.findAll().then( function(reclamos){
+                       res.render('index',{ title:'Proyecto Final',
+                         subtitle: 'Avances en computacion',
+                          reclamos: reclamos
+                             });
+                              });
+					 }
+exports.registroReclamo = function(req, res) {
+
+  var reclamo = {tipoReclamo:'Odeco',observacion:'',fecha:hoy}
+ 		              res.render('registroReclamo', {reclamo:reclamo});
+
+		   };
+exports.guardarReclamo = function(req, res) {
+
+	console.log("ingresa a guardar");
+	reclamo = models.reclamo.build (req.body.reclamo);
+    //a continuacion guardamos los campos que recibimos del formulario
+    reclamo.save	({fields:["tipoReclamo",
+    					      "observacion",
+    					      "fecha"
+    					  ]
+    			 }).then(function(){
+    			 	console.log("ingresa a redirect");
+    			 	res.render('respuestaReclamo', {mensaje:'Se registro el reclamo con exito'})
+    			 });
+console.log(reclamo);
+                                              };
+exports.buscaReclamo = function(req, res){
+  var tipoReclamoFiltro = req.body.tipoReclamoFiltro;
+  models.reclamo.findAll().then( function(reclamos){
+        res.render('respuestaBusqueda',{  tipoReclamoFiltro:tipoReclamoFiltro, reclamos:reclamos});
+               });
+}
+
+exports.buscarReclamo = function(req, res){
+  var tipoReclamoFiltro = 'ODECO';
+  models.reclamo.findAll().then( function(reclamos){
+        res.render('buscarReclamo',{  tipoReclamoFiltro:tipoReclamoFiltro, reclamos:reclamos});
+               });
+}
